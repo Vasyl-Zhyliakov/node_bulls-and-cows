bef755fd04670d835f34bad369fafb3a2b730d2e
Vasyl Zhyliakov <snak87@gmail.com>
2025-05-28 16:48:57 +0300

    solutin v 1.0

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@
 'use strict';
 
 const readline = require('readline');
-const generateRandomNumber = require('./modules/generateRandomNumber.js');
-const checkIsValidUserInput = require('./modules/checkIsValidUserInput.js');
-const getBullsAndCows = require('./modules/getBullsAndCows.js');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
 
 const newNumber = generateRandomNumber();
 
 const terminal = readline.createInterface(process.stdin, process.stdout);
 
-// console.log(
-//   'I created a number that includes 4 unique digits. Can you guess it?',
-// );
+console.log(
+  'I created a number that includes 4 unique digits. Can you guess it?',
+);
 
 function askPlayer() {
   terminal.question('Enter 4 unique digits', (digits) => {
@@ -20,7 +20,7 @@ function askPlayer() {
       askPlayer();
     }
 
-    if (digits === newNumber) {
+    if (+digits === newNumber) {
       console.log('You win. Congratulations!!!');
 
       terminal.close();
