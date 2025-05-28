c53c70d7efd926a29604641a147e23b6fa94d71b
Vasyl Zhyliakov <snak87@gmail.com>
2025-05-28 16:14:36 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const generateRandomNumber = require('./modules/generateRandomNumber.js');
+const checkIsValidUserInput = require('./modules/checkIsValidUserInput.js');
+const getBullsAndCows = require('./modules/getBullsAndCows.js');
+
+const newNumber = generateRandomNumber();
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+
+// console.log(
+//   'I created a number that includes 4 unique digits. Can you guess it?',
+// );
+
+function askPlayer() {
+  terminal.question('Enter 4 unique digits', (digits) => {
+    if (!checkIsValidUserInput(digits)) {
+      askPlayer();
+    }
+
+    if (digits === newNumber) {
+      // console.log('You win. Congratulations!!!');
+      terminal.close();
+    } else {
+      getBullsAndCows(digits, newNumber);
+      askPlayer();
+    }
+  });
+}
+
+askPlayer();
